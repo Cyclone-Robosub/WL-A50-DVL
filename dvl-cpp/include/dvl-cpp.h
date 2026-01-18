be2214346d9b31118ccdeda291d3f054badd7559
KoryJHaydon <138720816+KoryJHaydon@users.noreply.github.com>
2026-01-17 21:39:32 -0800

    Fixes to api, added some commands

--- dvl-cpp/include/dvl-cpp.h
+++ dvl-cpp/include/dvl-cpp.h
@@ -8,6 +8,7 @@
 #include <iomanip>
 #include <stdexcept>
 #include "serial/serial.h" //https://github.com/wjwwood/serial
+#include <chrono>
 
 //TODO - add check to make sure awk is received from the DVL
 //TODO - add status parsing for VR and DRR
@@ -20,25 +21,27 @@ class DVL {
         DVL(const std::string& port, unsigned long baudrate = 115200);
 
         struct VR {
-            float vx = 0, vy = 0, vz = 0, altitude = 0, fom = 0, covariance = 0, time_of_validity = 0, time_of_transmission = 0, time = 0;
-            bool valid = false;
-            uint8_t status = 0;
+            float vx = 0, vy = 0, vz = 0, altitude = 0, fom = 0, time = 0;
+            std::array<float,9> covariance = {0};
+            int64_t time_of_validity = 0, time_of_transmission = 0;
+            char valid = 'n';
+            uint8_t status = 0x00; 
         };
 
         struct DRR {
-            float time_stamp = 0, x = 0, y = 0, z = 0, pos_std = 0, roll = 0, pitch = 0, yaw = 0;
-            bool status = false;
+            int64_t time_stamp = 0;
+            float x = 0, y = 0, z = 0, pos_std = 0, roll = 0, pitch = 0, yaw = 0;
+            uint8_t status = 0x00;
         };
 
         struct Config {
             float speed_of_sound;
             float mounting_rotation_offset;
-            char acoustic_enabled;
-            char dark_mode_enabled;
+            char acoustic_enabled; //y or n
+            char dark_mode_enabled; //y or n
             std::string range_mode = "auto";
-            bool periodic_cycling_enabled = true;
-
-        }
+            char periodic_cycling_enabled; //y or n
+        };
 
         // PUBLIC API //
         //reads
@@ -52,10 +55,10 @@ class DVL {
         
 
         //sets (returns true if acknowledge was received)
-        bool setConfig(float speed_of_sound, float mounting_rotation_offset, char acoustic_enabled, char dark_mode_enabled, std::string range_mode, bool periodic_cycling_enabled);
+        bool setConfig(float, float, char, char, std::string, bool);
         bool resetDRR(); //reset the dead reckoning report
         bool resetGyro(); //zero the gyroscope
-        bool setSerProtocol(uint8_t);
+        bool setSerProtocol(int);
 
         
     private:
@@ -68,7 +71,7 @@ class DVL {
         // PRIVATE CONSTANTS //
         // Protocol definitions
         static constexpr uint8_t SOP = 'w'; //start of package
-        static constexpr std::vector<uint8_t> EOP = {'\n','\r'}; //possible end of packets bytes
+        static constexpr std::array<uint8_t,2> EOP = {'\n','\r'}; //possible end of packets bytes
         static constexpr uint8_t DIR_CMD = 'c'; //command
         static constexpr uint8_t DIR_RESP = 'r'; //response
         static constexpr uint8_t CS = '*'; //checksum 
@@ -87,6 +90,8 @@ class DVL {
         static constexpr uint8_t REC_TR = 'u'; //receive transducer report
         static constexpr uint8_t REC_DRR = 'p'; //receive dead reckoning report
 
+        static constexpr uint8_t ACK = 'a';
+
         // list of valid outgoing commands
         static constexpr std::array<char,8> VALID_OUT = {CMD_GET_VERSION, CMD_GET_PRODUCT_DETAIL, CMD_SET_SETTINGS, CMD_GET_SETTINGS, CMD_RESET_DR, CMD_TRIGGER_PING, CMD_CALIBRATE_GYRO, CMD_CHANGE_SER_OUTPUT};
 
@@ -95,16 +100,24 @@ class DVL {
 
         // PRIVATE VARS //
         serial::Serial ser;
-        VR lastVelocityReport;
-        DRR lastDRReport;
+        VR vr, error_vr;
+        DRR drr, error_drr;
 
+        Config config, error_config;
+        
+      
+        
+        std::string version; //Protocol version "major.minor.patch"
+        std::string product_details;
 
         // PRIVATE METHODS //
-        void parse(const std::vector<uint8_t>& sentence); //parses text string from DVL into the results structure
-        std::string readLine(); //reads until /r/n
+        bool parseResponse(std::string&); //parses text string from DVL into the results structure
+        bool holdForResponse(const char); 
+        bool sendCommand(uint8_t, const std::vector<std::string>& = {});
+
 
         // CRC-8 MAXIM lookup table
-    constexpr uint8_t CRC8_TABLE[256] = {
+    static constexpr uint8_t CRC8_TABLE[256] = {
         0x00,0x31,0x62,0x53,0xC4,0xF5,0xA6,0x97,0xB9,0x88,0xDB,0xEA,0x7D,0x4C,0x1F,0x2E,
         0x43,0x72,0x21,0x10,0x87,0xB6,0xE5,0xD4,0xFA,0xCB,0x98,0xA9,0x3E,0x0F,0x5C,0x6D,
         0x86,0xB7,0xE4,0xD5,0x42,0x73,0x20,0x11,0x3F,0x0E,0x5D,0x6C,0xFB,0xCA,0x99,0xA8,
